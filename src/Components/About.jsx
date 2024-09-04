5d28deb27f3f73d9975e3da7b74472310922a2da
AnuragKush2529 <akush2503@gmail.com>
2024-09-04 17:43:59 +0530

    Completed all the sections, made the website responsive and added animations.

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,48 +1,129 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const About = () => {
+  const [isLoaded, setLoaded] = useState(false);
+  const [isLoaded2, setLoaded2] = useState(false);
+  const [isLoaded3, setLoaded3] = useState(false);
+  const [isLoaded4, setLoaded4] = useState(false);
+  const [isLoaded5, setLoaded5] = useState(false);
+
+  const rotatingTexts = [
+    "Problem Solver",
+    "Software Developer",
+    "Full Stack Developer",
+    "Programmer",
+  ];
+
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
+  const [textVisible, setTextVisible] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTextVisible(false);
+      setTimeout(() => {
+        setCurrentTextIndex(
+          (prevIndex) => (prevIndex + 1) % rotatingTexts.length
+        );
+        setTextVisible(true);
+      }, 1000);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 100);
+    setTimeout(() => {
+      setLoaded2(true);
+    }, 200);
+    setTimeout(() => {
+      setLoaded3(true);
+    }, 300);
+    setTimeout(() => {
+      setLoaded4(true);
+    }, 400);
+    setTimeout(() => {
+      setLoaded5(true);
+    }, 400);
+  }, []);
+
   return (
     <div
       className="flex flex-col-reverse mx-20 md:flex-row md:justify-around md:mx-0 md:px-14 md:p-2 md:pt-20 lg:pt-24 pb-20 lg:px-44"
       id="aboutPage"
     >
-      <div className="md:w-1/2 flex flex-col items-center md:flex-none md:items-start">
-        <h3 className="pt-0 text-2xl text-white">
+      <div
+        className={`md:w-1/2 flex flex-col items-center md:flex-none md:items-start`}
+      >
+        <h3
+          className={`pt-0 text-2xl text-white transition-transform duration-700 ease-in-out ${
+            isLoaded ? "transform translate-y-0" : "transform translate-y-16"
+          }`}
+        >
           Hello, <span className="text-sky-500">I'm</span>
         </h3>
-        <h1 className="pt-6 pb-2 text-3xl md:text-5xl font-medium text-sky-500">
+        <h1
+          className={`pt-6 pb-2 text-3xl md:text-5xl font-medium text-sky-1000 transition-transform duration-700 ease-in-out ${
+            isLoaded2 ? "transform translate-y-0" : "transform translate-y-16"
+          }`}
+        >
           Anurag Kushwaha
         </h1>
-        <h3 className="mt-8 text-xl md:text-2xl font-medium text-white">
-          I am into Web Development.
+        <h3
+          className={`mt-8 text-xl md:text-2xl font-medium text-white transition-transform duration-700 ease-in-out ${
+            isLoaded3 ? "transform translate-y-0" : "transform translate-y-16"
+          }`}
+        >
+          I am a{" "}
+          <span
+            className={`text-2xl text-cyan-300 fade-in ${
+              textVisible ? "fade-in-visible" : ""
+            }`}
+          >
+            {rotatingTexts[currentTextIndex]}.{" "}
+          </span>
         </h3>
-        <p className="pt-12 text-white tracking-wide">
+        <p
+          className={`pt-12 text-white tracking-wide transition-transform duration-700 ease-in-out ${
+            isLoaded4 ? "transform translate-y-0" : "transform translate-y-16"
+          }`}
+        >
           An enthusiastic individual with good problem solving skills and
           passion about learning new things. Aspiring Software Engineer with a
           strong foundation in software development, algorithms and data
           structures.
         </p>
-        <button className="contact-btn bg-sky-500 text-white font-bold ml-0.5 mt-16 py-3 px-8 rounded-2xl hover:bg-[#020020] hover:text-sky-500 hover:outline">
+        <button
+          className={`contact-btn bg-sky-500 text-white font-bold ml-0.5 mt-16 py-3 px-8 rounded-2xl hover:bg-[#020020] hover:text-sky-500 hover:outline transition-transform duration-700 ease-in-out ${
+            isLoaded5 ? "transform translate-y-0" : "transform translate-y-16"
+          }`}
+        >
           <a href="#contactPage">Let's Talk</a>
         </button>
       </div>
-      <div className="flex flex-col items-center d:w-1/2 mb-20 mt-12 md:mt-0 md:mb-0">
+      <div
+        className={`flex flex-col items-center d:w-1/2 mb-20 mt-12 pr-6 md:mt-0 md:mb-0 transition-transform duration-1000 ease-out ${
+          isLoaded4 ? "transform translate-y-0" : "transform translate-y-16"
+        }`}
+      >
         <img
-          className="profile-pic md:ml-auto md:mr-4 rounded-2xl border-0 border-sky-500"
-          src="ProfilePic.jpg"
+          className="profile-pic md:ml-auto md:mr-4 rounded-2xl border-0 border-sky-500 brightness-90"
+          src="ProfilePic3.jpg"
           height="300px"
-          width="300px"
+          width="260px"
         ></img>
         <div className="flex mt-16 md:mt-12 md:ml-20 lg:mt-2 lg:ml-60">
-          <p className="check m-2 mt-4 md:ml-9 md:mt-6 text-sky-500">
-            Check Out My
+          <p className="check m-2 mt-4 ml-0 md:ml-4 md:mt-5 text-sky-500">
+            Check my
           </p>
           <a
             href="https://www.linkedin.com/in/anurag-kushwaha-463796228/"
             target="_blank"
           >
             <img
-              className="cntele bg-sky-600 m-4 p-1 rounded-xl"
+              className="cntele bg-sky-600 m-4 p-1 rounded-xl hover:bg-white"
               src="linkedin.svg"
               height="30px"
               width="30px"
@@ -50,16 +131,16 @@ const About = () => {
           </a>
           <a href="https://github.com/AnuragKush2527/" target="_blank">
             <img
-              className="cntele bg-sky-600 m-4 p-1 rounded-xl"
+              className="cntele bg-sky-600 m-4 p-1 rounded-xl hover:bg-white"
               src="github.svg"
               height="30px"
               width="30px"
             />
           </a>
-          <a href="mailto:akush2503@gmail.com">
+          <a href="https://www.instagram.com/anurag_kush2503/" target="_blank">
             <img
-              className="cntele bg-sky-600 m-4 p-1 rounded-xl"
-              src="gmail.svg"
+              className="cntele bg-sky-600 m-4 p-1 rounded-xl hover:bg-white"
+              src="instagram.svg"
               height="30px"
               width="30px"
             ></img>
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,43 +1,108 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Heading from "./Heading";
+import emailjs from "@emailjs/browser";
+import CustomAlert from "./CustomAlert";
 
 const Contact = () => {
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+  const [alertType, setAlertType] = useState("");
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_SERVICE_ID,
+        import.meta.env.VITE_TEMPLATE_ID,
+        form.current,
+        import.meta.env.VITE_USER_ID
+      )
+      .then(
+        (result) => {
+          console.log("Email successfully sent!", result.text);
+          // alert("Message sent successfully!");
+
+          setAlertMessage("Message sent successfully!");
+          setAlertType("success");
+          setShowAlert(true);
+        },
+        (error) => {
+          console.log("Failed to send email.", error.text);
+          // alert("Failed to send message, please try again.");
+
+          setAlertMessage("Failed to send Email. Please try again.");
+          setAlertType("error");
+          setShowAlert(true);
+        }
+      );
+    e.target.reset();
+  };
+
+  const handleShowAlert = () => {
+    setShowAlert(true);
+    setTimeout(() => setShowAlert(false), 3000); // Auto close after 3 seconds
+  };
+
+  const sendEmailToast = (e) => {
+    handleShowAlert();
+    sendEmail(e);
+  };
+
   return (
     <>
       <Heading heading="Contact" ids="contactPage" />
-      <div className="flex justify-center text-white">
+      <div className="flex justify-center text-white mt-[-50px] md:mt-0 ">
         <div className="contact-div flex justify-center w-80 md:w-1/3">
-          <form method="post" action="">
+          <form ref={form} onSubmit={sendEmailToast}>
             <input
               className="inputs my-4 p-2 w-80 md:w-full rounded-lg custom-placeholder"
-              type="text"
+              type="email"
+              name="user_email"
               placeholder="Your Email"
+              required
             ></input>
             <input
               className="inputs my-4 p-2 w-80 md:w-full rounded-lg custom-placeholder"
               type="text"
+              name="user_name"
               placeholder="Your Name"
+              required
             ></input>
             <input
               className="inputs my-4 p-2 w-80 md:w-full rounded-lg custom-placeholder"
               type="text"
+              name="subject"
               placeholder="Subject"
+              required
             ></input>
             <textarea
               className="inputs my-4 mb-0 p-2 w-80 md:w-full rounded-lg custom-placeholder"
               rows="6"
               type="text"
+              name="message"
               placeholder="Message"
+              required
             ></textarea>
             <button
               className="contact-btn bg-sky-500 text-white font-bold mt-8 py-3 px-8 rounded-2xl hover:bg-[#020020] hover:text-sky-500 hover:outline"
-              type="button"
+              type="submit"
             >
               Send Message
             </button>
           </form>
         </div>
       </div>
+
+      {showAlert && (
+        <CustomAlert
+          message={alertMessage}
+          type={alertType}
+          onClose={() => setShowAlert(false)}
+        />
+      )}
     </>
   );
 };
